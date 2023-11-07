42db93e711b5ebf6bede2f93215c317f70be697d
pepperoni21 <pepperoni2100@gmail.com>
2023-11-07 15:54:45 +0100

    Added pull model test + added PullModelStatus missing fields

---
+++ tests/pull_model.rs
@@ -0,0 +1,13 @@
+use ollama_rs::Ollama;
+use tokio_stream::StreamExt;
+
+#[tokio::test]
+async fn test_pull_model() {
+    let ollama = Ollama::default();
+
+    let mut res = ollama.pull_model_stream("llama2:latest".into(), false).await.unwrap();
+
+    while let Some(Ok(res)) = res.next().await {
+        dbg!(res);
+    }
+}
