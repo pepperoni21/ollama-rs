eb448cf6927899ef27b9d8ead51c05f1c871c022
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-11-23 11:29:18 +1100

    Public Image base64 content

--- src/generation/images.rs
+++ src/generation/images.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Image(String);
+pub struct Image(pub String);
 
 impl Image {
     pub fn from_base64(base64: &str) -> Self {
