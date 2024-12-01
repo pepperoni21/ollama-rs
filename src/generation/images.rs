722713158979c19a847b11869641f8bdaf5ea84e
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-12-01 14:20:46 +1100

    Fix build

--- src/generation/images.rs
+++ src/generation/images.rs
@@ -5,7 +5,7 @@ pub struct Image(String);
 
 impl Image {
     pub fn from_base64(base64: impl Into<String>) -> Self {
-        Self(base64.to_string())
+        Self(base64.into())
     }
 
     pub fn to_base64(&self) -> &str {
