ded23a98550d66ce948236c30e450b9778eb63ed
Roel Kluin <r.kluin@nki.nl>
2025-02-25 10:28:32 +0100

    add modelfile feature, to use the modelfile crate

--- ollama-rs/src/models.rs
+++ ollama-rs/src/models.rs
@@ -10,6 +10,12 @@ pub mod pull;
 pub mod push;
 pub mod show_info;
 
+#[cfg(feature = "modelfile")]
+use modelfile::modelfile::Modelfile;
+
+#[cfg(feature = "modelfile")]
+use serde_with;
+
 use serde::{Deserialize, Serialize};
 
 /// Represents a local model pulled from Ollama.
@@ -28,10 +34,15 @@ pub struct LocalModel {
 /// This struct contains various fields that describe a model's attributes,
 /// such as its license, file, parameters, and template.
 /// Some fields may be empty if the model does not have them.
+#[cfg_attr(feature = "modelfile", serde_with::serde_as)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ModelInfo {
     #[serde(default = "String::new")]
     pub license: String,
+    #[cfg(feature = "modelfile")]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
+    pub modelfile: Modelfile,
+    #[cfg(not(feature = "modelfile"))]
     #[serde(default = "String::new")]
     pub modelfile: String,
     #[serde(default = "String::new")]
