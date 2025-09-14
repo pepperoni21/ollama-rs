6d91aaf0f0b9ac45a8738ee724cfc3040b813543
Markus Ineichen <kusi.ineichen@gmail.com>
2025-09-15 00:29:20 +0200

    Avoid using blocking io in async code

--- ollama-rs/examples/basic_chatbot.rs
+++ ollama-rs/examples/basic_chatbot.rs
@@ -1,33 +1,41 @@
+use futures_util::TryStreamExt;
 use ollama_rs::{
     generation::completion::{
         request::GenerationRequest, GenerationContext, GenerationResponseStream,
     },
     Ollama,
 };
-use tokio::io::{stdout, AsyncWriteExt};
+use tokio::io::{stdout, AsyncBufReadExt, AsyncWriteExt};
 use tokio_stream::StreamExt;
+
+const MODEL: &str = "llama2:latest";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = std::env::args().skip(1).fuse();
+    let model = args.next().unwrap_or_else(|| MODEL.to_string());
+
     let ollama = Ollama::default();
 
     let mut stdout = stdout();
-
+    let mut commands = std::pin::pin!({
+        let stdin_reader = tokio::io::BufReader::new(tokio::io::stdin());
+        let line_stream = tokio_stream::wrappers::LinesStream::new(stdin_reader.lines());
+        line_stream.try_take_while(|x| std::future::ready(Ok(!x.eq_ignore_ascii_case("exit"))))
+    });
     let mut context: Option<GenerationContext> = None;
 
     loop {
         stdout.write_all(b"\n> ").await?;
         stdout.flush().await?;
 
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input)?;
+        let input = match commands.next().await {
+            Some(Ok(x)) => x,
+            Some(Err(e)) => return Err(e.into()),
+            None => break,
+        };
 
-        let input = input.trim_end();
-        if input.eq_ignore_ascii_case("exit") {
-            break;
-        }
-
-        let mut request = GenerationRequest::new("llama2:latest".into(), input.to_string());
+        let mut request = GenerationRequest::new(model.clone(), input);
         if let Some(context) = context.clone() {
             request = request.context(context);
         }
@@ -37,10 +45,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             for ele in res {
                 stdout.write_all(ele.response.as_bytes()).await?;
                 stdout.flush().await?;
-
-                if ele.context.is_some() {
-                    context = ele.context;
-                }
+                context = ele.context.or(context);
             }
         }
     }
