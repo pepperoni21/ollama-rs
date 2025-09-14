66631e75f55cad5bd54779f52e94b0ab450e2093
Markus Ineichen <kusi.ineichen@gmail.com>
2025-09-14 21:44:35 +0200

    Remove meaningless comments and add possibility to provide custom model and image url

--- ollama-rs/examples/images_to_ollama.rs
+++ ollama-rs/examples/images_to_ollama.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use base64::Engine;
 use ollama_rs::{
     generation::{
@@ -7,56 +9,34 @@ use ollama_rs::{
     Ollama,
 };
 use reqwest::get;
-use tokio::runtime::Runtime;
 
 const IMAGE_URL: &str = "https://images.pexels.com/photos/1054655/pexels-photo-1054655.jpeg";
 const PROMPT: &str = "Describe this image";
 
-fn main() {
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async {
-        // Download the image and encode it to base64
-        let bytes = match download_image(IMAGE_URL).await {
-            Ok(b) => b,
-            Err(e) => {
-                eprintln!("Failed to download image: {e}");
-                return;
-            }
-        };
-        let base64_image = base64::engine::general_purpose::STANDARD.encode(&bytes);
+/// Usage: cargo run --example images_to_ollama -- https://assets.canarymedia.com/content/uploads/Alex-honnold-lead-resized.jpg
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = std::env::args().skip(1).fuse();
+    let image_url = args.next().unwrap_or_else(|| IMAGE_URL.into());
+    let model = args.next().unwrap_or_else(|| "llava:latest".to_string());
+    let image = download_image(&image_url).await?;
+    let request = GenerationRequest::new(model, PROMPT).add_image(image);
+    let response = send_request(request).await?;
 
-        // Create an Image struct from the base64 string
-        let image = Image::from_base64(&base64_image);
-
-        // Create a GenerationRequest with the model and prompt, adding the image
-        let request =
-            GenerationRequest::new("llava:latest".to_string(), PROMPT.to_string()).add_image(image);
-
-        // Send the request to the model and get the response
-        let response = match send_request(request).await {
-            Ok(r) => r,
-            Err(e) => {
-                eprintln!("Failed to get response: {e}");
-                return;
-            }
-        };
-
-        // Print the response
-        println!("{}", response.response);
-    });
+    println!("{}", response.response);
+    Ok(())
 }
 
-// Function to download the image
-async fn download_image(url: &str) -> Result<Vec<u8>, reqwest::Error> {
+async fn download_image(url: &str) -> Result<Image, reqwest::Error> {
     let response = get(url).await?;
     let bytes = response.bytes().await?;
-    Ok(bytes.to_vec())
+    let base64_image = base64::engine::general_purpose::STANDARD.encode(&bytes);
+    Ok(Image::from_base64(&base64_image))
 }
 
-// Function to send the request to the model
 async fn send_request(
     request: GenerationRequest<'_>,
-) -> Result<GenerationResponse, Box<dyn std::error::Error>> {
+) -> Result<GenerationResponse, Box<dyn Error>> {
     let ollama = Ollama::default();
     let response = ollama.generate(request).await?;
     Ok(response)
