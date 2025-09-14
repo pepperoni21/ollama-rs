5269b6db7968e6448fbe806bc2dabf6b9066fbca
Markus Ineichen <kusi.ineichen@gmail.com>
2025-09-15 00:34:25 +0200

    Avoid using blocking io

--- ollama-rs/examples/coordinator.rs
+++ ollama-rs/examples/coordinator.rs
@@ -1,6 +1,7 @@
-use std::io::{stdin, stdout, Write};
-
+use futures_util::TryStreamExt;
 use ollama_rs::{coordinator::Coordinator, generation::chat::ChatMessage, Ollama};
+use tokio::io::{stdout, AsyncBufReadExt, AsyncWriteExt};
+use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,25 +9,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let history = vec![];
     let mut coordinator = Coordinator::new(ollama, "llama3.2".to_string(), history);
 
-    let stdin = stdin();
+    let mut commands = std::pin::pin!({
+        let stdin_reader = tokio::io::BufReader::new(tokio::io::stdin());
+        let line_stream = tokio_stream::wrappers::LinesStream::new(stdin_reader.lines());
+        line_stream.try_take_while(|x| std::future::ready(Ok(!x.eq_ignore_ascii_case("exit"))))
+    });
     let mut stdout = stdout();
     loop {
-        stdout.write_all(b"\n> ")?;
-        stdout.flush()?;
+        stdout.write_all(b"\n> ").await?;
+        stdout.flush().await?;
 
-        let mut input = String::new();
-        stdin.read_line(&mut input)?;
+        let input = match commands.next().await {
+            Some(Ok(x)) => x,
+            Some(Err(e)) => return Err(e.into()),
+            None => break,
+        };
 
-        let input = input.trim_end();
-        if input.eq_ignore_ascii_case("exit") {
-            break;
-        }
-
-        let resp = coordinator
-            .chat(vec![ChatMessage::user(input.to_string())])
-            .await?;
-
-        println!("{}", resp.message.content);
+        let resp = coordinator.chat(vec![ChatMessage::user(input)]).await?;
+        stdout.write_all(resp.message.content.as_bytes()).await?;
     }
 
     Ok(())
